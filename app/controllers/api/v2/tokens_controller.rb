2ecac00ebf7dd8dfa53ec1b25027d2439027b406
Christoph Honal <christoph.honal@web.de>
2020-02-19 03:23:00 +0100

    fix size calculation

--- app/controllers/api/v2/tokens_controller.rb
+++ app/controllers/api/v2/tokens_controller.rb
@@ -28,7 +28,6 @@ class Api::V2::TokensController < Api::BaseController
     auth_scopes = []
     auth_scopes = authorize_scopes(registry) unless registry.nil?
 
-    logger.info "CANARY: #{current_user.username}"
     token = Portus::JwtToken.new(current_user.username, params[:service], auth_scopes)
     logger.tagged("jwt_token", "claim") { logger.debug token.claim }
     render json: token.encoded_hash
